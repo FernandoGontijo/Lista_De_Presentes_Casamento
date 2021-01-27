c52b98cfb0b941f9f00ae0d0c5875dc0cc2d741c
Fernando <fernando.gontijos@hotmail.com>
2021-01-27 19:02:57 -0300

    implementação navbar

--- casamento-web/src/app/app-routing.module.ts
+++ casamento-web/src/app/app-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { ListaPresenteComponent } from './views/lista-presente/lista-presente.component';
+import { NossaHistoriaComponent } from './views/nossa-historia/nossa-historia.component';
+import { CerimoniaComponent } from './views/cerimonia/cerimonia.component';
+import { MensagensComponent } from './views/mensagens/mensagens.component';
 
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent
+    path: '', redirectTo: '/home', pathMatch:'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'historia', component: NossaHistoriaComponent
+  },
+  {
+    path: 'presentes', component: ListaPresenteComponent
+  },
+  {
+    path: 'cerimonia', component: CerimoniaComponent
+  },
+  {
+    path: 'mensagens', component: MensagensComponent
   }
 ];
 
