74d45f5761fc70ccf6ae35509da966709cd309be
Fernando <fernando.gontijos@hotmail.com>
2021-01-27 23:42:07 -0300

    implementação da barra de navegação

--- casamento-web/src/app/app.module.ts
+++ casamento-web/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './views/home/home.component';
 import { ListaPresenteComponent } from './views/lista-presente/lista-presente.component';
+import { CerimoniaComponent } from './views/cerimonia/cerimonia.component';
+import { MensagensComponent } from './views/mensagens/mensagens.component';
+import { NossaHistoriaComponent } from './views/nossa-historia/nossa-historia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ListaPresenteComponent
+    ListaPresenteComponent,
+    CerimoniaComponent,
+    MensagensComponent,
+    NossaHistoriaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
+ 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
